e3f3be00ab875f06ce86752ec0fb6065b873d7e5
alexwatts83 <alexjwatts83@gmail.com>
2020-04-01 21:52:07 +1100

    added reverse int function

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,22 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  let str = n.toString().replace('-', '');
+  let len = str.length - 1;
+  let result = 0;
+
+  for(let i = len; i >= 0; i--) {
+    if (str[i]==='-'){
+      result = result * - 1;
+      continue;
+    }
+    let pow = Math.pow(10, i);
+    let num = parseInt(str[i]);
+    result = (pow * num) + result;
+  }
+
+  return (n >= 0) ? result : -result;
+}
 
 module.exports = reverseInt;
