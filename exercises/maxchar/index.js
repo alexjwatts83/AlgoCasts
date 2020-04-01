88072fd2a1d25a0e32aa8d4ca6314c2003ded8e9
alexwatts83 <alexjwatts83@gmail.com>
2020-04-01 23:03:27 +1100

    added my version of max chars solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,31 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let cache = {};
+  let n = str.length;
+
+  for(let i = 0; i < n; i++) {
+    let c = str[i];
+    if(cache[c]) {
+      cache[c] = cache[c] + 1;
+    } else {
+      cache[c] = 1;
+    }
+  }
+
+  let maxCount = 0;
+  let charWithMostCounts = '';
+  
+  for(var prop in cache) {
+    let count = cache[prop];
+    if (count > maxCount){
+      charWithMostCounts = prop;
+      maxCount = count;
+    }
+  }
+
+  return charWithMostCounts;
+}
 
 module.exports = maxChar;
