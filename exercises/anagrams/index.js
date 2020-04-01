7fa7b317304759605255b54227c474f51c367056
alexwatts83 <alexjwatts83@gmail.com>
2020-04-02 07:37:33 +1100

    first pass at anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,54 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  let mappedCharsA = mapString(stringA);
+  let mappedCharsB = mapString(stringB);
+  let stringAChars = [];
+
+  for(var charA in mappedCharsA) {
+    // if the charA is in stringB
+    if (!mappedCharsB[charA]) {
+      console.log(`${stringB} does not contain ${charA}`);
+      return false;
+    }
+
+    if (mappedCharsA[charA] !== mappedCharsB[charA]) {
+      console.log(`Char Count for ${charA} does not match.`);
+      return false;
+    }
+  }
+
+  for(var charB in mappedCharsB) {
+    // if the charB is in stringA
+    if (!mappedCharsA[charB]) {
+      console.log(`${stringB} does not contain ${charB}`);
+      return false;
+    }
+
+    if (mappedCharsA[charB] !== mappedCharsB[charB]) {
+      console.log(`Char Count for ${charB} does not match.`);
+      return false;
+    }
+  }
+  
+
+  return true;
+}
+
+function mapString(str) {
+  str = str.replace(/[^\w]/g, '').toLowerCase();
+  // console.log('str' + str);
+  let cache = {};
+  let n = str.length;
+
+  for(let i = 0; i < n; i++) {
+    let c = str[i];
+    let count = cache[c] + 1 || 1;
+
+    cache[c] = count;
+  }
+  return cache;
+}
 
 module.exports = anagrams;
