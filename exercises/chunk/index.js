e035e8b9e8a336af3870f9d78df83c8b3b929a9f
alexwatts83 <alexjwatts83@gmail.com>
2020-04-01 23:50:34 +1100

    chunk initial solution done

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,28 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  let n = array.length;
+  let chunked = [];
+  for(let i = 0; i < n; i++) {
+    const value = array[i];
+    // console.log({i:i,chunked:chunked});
+    if (!chunked[chunked.length-1]) {
+      chunked.push([value]);
+      continue;
+    }
+    // console.log('get here');
+    let arr = chunked[chunked.length-1];
+    let arrLen = arr.length;
+    // console.log({i:i,arr:arr,arrLen:arrLen});
+    if (arrLen < size) {
+      arr.push(value);
+    } else {
+      chunked.push([value]);
+    }
+  }
+
+  return chunked;
+}
 
 module.exports = chunk;
