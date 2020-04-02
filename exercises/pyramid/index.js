be6859f0f80e485422f829dbb827d7194d4975d7
alexwatts83 <alexjwatts83@gmail.com>
2020-04-02 22:45:19 +1100

    added pyramid solution

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -1,4 +1,4 @@
-// --- Directions
+;// --- Directions
 // Write a function that accepts a positive number N.
 // The function should console log a pyramid shape
 // with N levels using the # character.  Make sure the
@@ -13,7 +13,48 @@
 //       '  #  '
 //       ' ### '
 //       '#####'
+//   pyramid(4)
+//       '   #   '
+//       '  ###  '
+//       ' ##### '
+//       '#######'
 
-function pyramid(n) {}
+function pyramid(n) {
+  // console.log(`pyramid => ${n}`);
+  let colCount = (2 * n) - 1;
+  var midPoint = Math.floor(colCount / 2);
+  // console.log(`pyramid of ${n} rows and ${colCount} columns`);
+  for(let row = 0; row < n; row++) {
+    let str = '';
+    for(let col = 0; col < colCount; col++) {
+      let inRangeLeft = col < midPoint - row;
+      let inRangeRight = col > midPoint + row;
+
+      if (inRangeLeft || inRangeRight){
+        str += ' ';
+      } else {
+        str += '#';
+      }
+    }
+    console.log(str);
+  }
+}
+
+function pyramidw(n) {
+  let colCount = (2 * n) - 1;
+  console.log(`pyramid of ${n} rows and ${colCount} columns`);
+  for(let row = 1; row <= n; row++) {
+    let str = '';
+    for(let col = 1; col <= colCount; col++) {
+      // if (col < row) {
+      //   str += '#';
+      // } else {
+      //   str += '-';
+      // }
+      str +=`[${row},${col}] `;
+    }
+    console.log(str);
+  }
+}
 
 module.exports = pyramid;
--- exercises/pyramid/test.js
+++ exercises/pyramid/test.js
@@ -35,3 +35,24 @@ test('prints a pryamid for n = 4', () => {
   expect(console.log.mock.calls[3][0]).toEqual('#######');
   expect(console.log.mock.calls.length).toEqual(4);
 });
+
+test('prints a pryamid for n = 5', () => {
+  pyramid(5);
+  expect(console.log.mock.calls[0][0]).toEqual('    #    ');
+  expect(console.log.mock.calls[1][0]).toEqual('   ###   ');
+  expect(console.log.mock.calls[2][0]).toEqual('  #####  ');
+  expect(console.log.mock.calls[3][0]).toEqual(' ####### ');
+  expect(console.log.mock.calls[4][0]).toEqual('#########');
+  expect(console.log.mock.calls.length).toEqual(5);
+});
+
+test('prints a pryamid for n = 6', () => {
+  pyramid(6);
+  expect(console.log.mock.calls[0][0]).toEqual('     #     ');
+  expect(console.log.mock.calls[1][0]).toEqual('    ###    ');
+  expect(console.log.mock.calls[2][0]).toEqual('   #####   ');
+  expect(console.log.mock.calls[3][0]).toEqual('  #######  ');
+  expect(console.log.mock.calls[4][0]).toEqual(' ######### ');
+  expect(console.log.mock.calls[5][0]).toEqual('###########');
+  expect(console.log.mock.calls.length).toEqual(6);
+});
