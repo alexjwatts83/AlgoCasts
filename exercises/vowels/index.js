c048ec9b44baa6decbbf6d1645f3613d3bb6b445
alexwatts83 <alexjwatts83@gmail.com>
2020-04-03 20:23:02 +1100

    added vowels solution

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,24 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  let count = 0;
+  let vowelsMap = {
+    a: true,
+    e: true,
+    i: true,
+    o: true,
+    u: true
+  };
+  let n = str.length;
+  for(let i = 0; i < n; i++) {
+    let c = str[i].toLowerCase();
+    if (vowelsMap[c]) {
+      count++;
+    }
+    // console.log(`c:${c};count:${count}`);
+  }
+  return count;
+}
 
 module.exports = vowels;
