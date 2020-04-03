11982bcfb8d780fc6298c65f8581f6e1617a9c8b
alexwatts83 <alexjwatts83@gmail.com>
2020-04-04 00:02:03 +1100

    added spiral solution

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,50 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  console.log(`matrix: n => ${n}`);
+  let spiral = [];
+  for(let i = 0; i < n; i++){
+    let lastIndex = spiral.push([]) -1;
+    for(let j = 0; j < n; j++){
+      spiral[lastIndex].push('*');
+    }
+  }
+  let rowStart = 0;
+  let colStart = 0;
+  let rowEnd = n - 1;
+  let colEnd = n - 1;
+  let num = 1;
+  while(num <= (n*n)){
+    // loop right
+    for (let col = colStart; col <= colEnd; col++) {
+      spiral[rowStart][col] = num;
+      num++;
+    }
+    // loop down
+    rowStart++;
+    for(let row = rowStart; row <= rowEnd; row++){
+      spiral[row][colEnd] = num;
+      num++;
+    }
+    colEnd--;
+    // loop left
+    for(let col = colEnd; col >= colStart; col--) {
+      spiral[rowEnd][col] = num;
+      num++;
+    }
+    // loop up
+    rowEnd--;
+    for(let row = rowEnd; row >= rowStart; row--) {
+      spiral[row][colStart] = num;
+      num++;
+    }
+    colStart++;
+    // console.log(`rowStart: ${rowStart}, rowEnd: ${rowEnd}`);
+    // console.log(`colStart: ${colStart}, rowEnd: ${colEnd}`);
+    // break;
+  }
+  return spiral;
+}
 
 module.exports = matrix;
