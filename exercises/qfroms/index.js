5f9b63bd729dbd878e0fff8c4690f0ebd07ff98d
alexwatts83 <alexjwatts83@gmail.com>
2020-04-04 20:24:57 +1100

    added queue using two stacks solution

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,51 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.stack1 = new Stack();
+    this.stack2 = new Stack();
+  }
+  add(num) {
+    // FIFO
+    this.stack1.push(num);
+  }
+  remove(){
+    /* Remove First Entry */
+
+    // Empty the first stack to get the first value
+    let first;
+    while(this.stack1.peek() !== undefined) {
+      first = this.stack1.pop();
+      this.stack2.push(first);
+    }
+
+    // re-add the values back into the first stack
+    // except for the first value
+    while(this.stack2.peek() !== undefined) {
+      let value = this.stack2.pop();
+      if (value !== first) {
+        this.stack1.push(value);
+      }
+    }
+    return first;
+  }
+  peek() {
+    /* view First Entry */
+
+    // Empty the first stack to get the first value
+    let first;
+    while(this.stack1.peek() !== undefined) {
+      first = this.stack1.pop();
+      this.stack2.push(first);
+    }
+    
+    // re-add the values back into the first stack
+    while(this.stack2.peek() !== undefined) {
+      this.stack1.push(this.stack2.pop());
+    }
+    return first;
+  }
+}
 
 module.exports = Queue;
