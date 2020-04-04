3a7d49833e7e5bc9a8bef09129241bbcaeffcd68
alexwatts83 <alexjwatts83@gmail.com>
2020-04-04 15:54:29 +1100

    basic b version of a queue, cheated a little because I used an array to stor the data

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,16 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.data = [];
+  }
+  add(num) {
+    this.data.push(num);
+  }
+  remove() {
+    return this.data.shift();
+  }
+}
 
 module.exports = Queue;
