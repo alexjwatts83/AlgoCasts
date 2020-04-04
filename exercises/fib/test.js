83e6e26764454eb4c7a1e90794d593f9cc37d5aa
alexwatts83 <alexjwatts83@gmail.com>
2020-04-04 15:35:42 +1100

    added more fib test cases

--- exercises/fib/test.js
+++ exercises/fib/test.js
@@ -4,22 +4,25 @@ test('Fib function is defined', () => {
   expect(typeof fib).toEqual('function');
 });
 
-test('calculates correct fib value for 1', () => {
-  expect(fib(1)).toEqual(1);
-});
-
-test('calculates correct fib value for 2', () => {
-  expect(fib(2)).toEqual(1);
-});
-
-test('calculates correct fib value for 3', () => {
-  expect(fib(3)).toEqual(2);
-});
-
-test('calculates correct fib value for 4', () => {
-  expect(fib(4)).toEqual(3);
-});
-
-test('calculates correct fib value for 15', () => {
-  expect(fib(39)).toEqual(63245986);
+const cases = [
+  [0, 0],
+  [1, 1],
+  [2, 1],
+  [3, 2],
+  [4, 3],
+  [10, 55],
+  [20, 6765],
+  [30, 832040],
+  [40, 102334155],
+  [50, 12586269025],
+  [60, 1548008755920],
+];
+describe("Fibonacci Sequence,", () => {
+  test.each(cases)(
+    "when n is %p, then result should be %p",
+    (num, result) => {
+      const fibResult = fib(num);
+      expect(fibResult).toEqual(result);
+    }
+  );
 });
