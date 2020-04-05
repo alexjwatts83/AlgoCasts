eab3801146ee2c1f8da24f9b55ea6df051e567be
alexwatts83 <alexjwatts83@gmail.com>
2020-04-05 21:07:21 +1000

    added level width solution using depth first search recursive

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,26 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  let levels = [];
+  let level = 1;
+  levels.push(1);
+  traverseDFRecursive(root, level, levels);
+
+  return levels;
+}
+
+function traverseDFRecursive(node, level, levels) {
+  let n = node.children.length;
+  if (n === 0) {
+    return;
+  }
+  levels[level] = (levels[level] || 0) + n;
+  level++;
+  for(let i = 0; i < node.children.length; i++) {
+    traverseDFRecursive(node.children[i], level, levels);
+  }
+  level--;
+}
 
 module.exports = levelWidth;
