0aac1e4cd8c38cd05e6d018c14aba40a120591d3
alexwatts83 <alexjwatts83@gmail.com>
2020-04-06 06:58:42 +1000

    added validate bst solution

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,39 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+// recursive solution
+// All nodes to the left must be less then the max
+// All nodes to the right must be greater than the min
+// The given node must be within the range
+
+function validate(node, min = null, max = null) {
+  // check if the value is within range
+  if (max !== null && node.data > max)
+  {
+    console.log(`!!! data > max ${node.data}, min = ${min} and max = ${max}`);
+    return false;
+  }
+
+  if (min !== null && node.data < min) {
+    console.log(`!!! data < min ${node.data}, min = ${min} and max = ${max}`);
+    return false
+  }
+
+  // validate the left node
+  if (node.left !== null) {
+    console.log(`<<< go left with ${node.left.data}, min = ${min} and max = ${max}`);
+    return validate(node.left, min, node.data);
+  }
+
+  // validate the right node
+  if (node.right !== null) {
+    // call the validate on the right
+    console.log(`>>> go right with ${node.right.data}, min = ${min} and max = ${max}`);
+    return validate(node.right, node.data, max);
+  }
+
+  console.log(`>><< within range ${node.data}, min = ${min} and max = ${max}`);
+  return true;
+}
 
 module.exports = validate;
--- exercises/validate/test.js
+++ exercises/validate/test.js
@@ -1,15 +1,71 @@
 const Node = require('./node');
 const validate = require('./index');
 
-test('Validate recognizes a valid BST', () => {
-  const n = new Node(10);
-  n.insert(5);
-  n.insert(15);
-  n.insert(0);
-  n.insert(20);
+describe('Validate identifies valid Binary Search Tree', () => {
+  test('When root is the only node, is valid', () => {
+    const n = new Node(10);  
+    expect(validate(n)).toEqual(true);
+  });
+  test('When root has only one left node', () => {
+    const n = new Node(10);  
+    n.insert(5);
+    expect(validate(n)).toEqual(true);
+  });
+  test('When root has only one right node', () => {
+    const n = new Node(10);  
+    n.insert(5);
+    expect(validate(n)).toEqual(true);
+  });
 
-  expect(validate(n)).toEqual(true);
-});
+  test('When root has both left and right node', () => {
+    const n = new Node(10);  
+    n.insert(5);
+    n.insert(15);
+    expect(validate(n)).toEqual(true);
+  });
+
+  test('Simple case', () => {
+    const n = new Node(10);
+    n.insert(5);
+    n.insert(15);
+    n.insert(0);
+    n.insert(20);
+    n.insert(25);
+  
+    expect(validate(n)).toEqual(true);
+  });
+
+  test('Bigger case 1', () => {
+    const n = new Node(10);
+    n.insert(5);
+    n.insert(15);
+    n.insert(0);
+    n.insert(20);
+    n.insert(-5);
+    n.insert(-2);
+    n.insert(25);
+    expect(validate(n)).toEqual(true);
+  });
+
+  test('Bigger case 2', () => {
+    const n = new Node(10);
+    n.insert(5);
+    n.insert(15);
+    n.insert(0);
+    n.insert(20);
+    n.insert(-5);
+    n.insert(-2);
+    n.insert(25);
+    n.insert(-10);
+    n.insert(-15);
+    n.insert(30);
+    n.insert(35);
+    expect(validate(n)).toEqual(true);
+  });
+
+})
+
+
 
 test('Validate recognizes an invalid BST', () => {
   const n = new Node(10);
