8313bbf4150fc9224be0937bd6102dc6d712b58b
alexwatts83 <alexjwatts83@gmail.com>
2020-04-07 23:19:31 +1000

    refactored minesweeper solution

--- exercises/minesweeper/index.js
+++ exercises/minesweeper/index.js
@@ -6,20 +6,37 @@ int[][] bombs1 = {{0, 2}, {2, 0}};
 //  [1, 2, 1],
 //  [-1, 1, 0]]
 */
-cache = {};
-
-function setCache(bombs) {
-  cache = {};
-  for(let k = 0; k < bombs.length; k++) {
-    let bombCoordinates = bombs[k];
-    let i = bombCoordinates[0];
-    let j = bombCoordinates[1];
-    cache[[i,j]] = true;
+function mineSweeper(bombs, numRows, numCols) {
+  // set the fields
+  var field = [];
+  for(let i = 0; i < numRows; i++) {
+    let n = field.push([]) - 1;
+    for(let j = 0; j < numCols; j++) {
+      field[n].push(0);
+    } 
   }
-  console.log({cache:cache});
+  for(let i = 0; i < bombs.length; i++){
+    // set the bomb cordinates
+    let x = bombs[i][0];
+    let y = bombs[i][1];
+    field[x][y] = -1;
+    for(let row = x - 1; row <= x + 1; row++){
+      for(let col = y - 1; col <= y + 1; col++) {
+        // check if inbounds
+        let isRowInbounds = row >= 0 && row < numRows;
+        let isColInbounds = col >= 0 && col < numCols;
+        if (isRowInbounds && isColInbounds && field[row][col] !== -1) {
+          field[row][col]++;
+        }
+      }
+    }
+  }
+  console.log(field);
+
+  return field;
 }
 
-function mineSweeper(bombs, numRows, numCols) {
+function mineSweeperCrap(bombs, numRows, numCols) {
   setCache(bombs);
   var field = [];
   for(let i = 0; i < numRows; i++) {
@@ -31,6 +48,19 @@ function mineSweeper(bombs, numRows, numCols) {
   console.log(field);
 
   return field;
+}
+
+cache = {};
+
+function setCache(bombs) {
+  cache = {};
+  for(let k = 0; k < bombs.length; k++) {
+    let bombCoordinates = bombs[k];
+    let i = bombCoordinates[0];
+    let j = bombCoordinates[1];
+    cache[[i,j]] = true;
+  }
+  console.log({cache:cache});
 }
 
 function bombCount(row, col, numRows, numCols) {
