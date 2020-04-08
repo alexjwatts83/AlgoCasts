91251e3fd0198d53f39177900c240ba393f94e01
alexwatts83 <alexjwatts83@gmail.com>
2020-04-08 21:42:29 +1000

    added solution for rotate using a new array not in place

--- exercises/rotate/index.js
+++ exercises/rotate/index.js
@@ -1,5 +1,30 @@
 function rotate(a, n) {
+  var rotated = [];
+
+  let len = n - 1;
+  // set the right hand side to 0
+  for(let i = 0; i < n; i++) {
+    let x = rotated.push([]);
+    for(let j = 0; j < n; j++){
+      rotated[x-1].push(0);
+    }
+  }
+
+  for(let i = 0; i < n; i++) {
+    for(let j = 0; j < n; j++){
+      rotated[j][n - 1 - i]= a[i][j];
+    }
+  }
+  
+  console.log({rotated:rotated});
+  return rotated;
+}
+
+function rotateInplace(a, n) {
   
 }
 
-module.exports = rotate;
+module.exports = {
+  rotate: rotate,
+  rotateInplace: rotateInplace
+};
--- exercises/rotate/test.js
+++ exercises/rotate/test.js
@@ -1,7 +1,13 @@
-const rotate = require('./index');
+const rotateModule = require('./index');
+const rotate = rotateModule.rotate;
+const rotateInplace = rotateModule.rotateInplace;
 
 test('rotate function exists', () => {
   expect(rotate).toBeDefined();
+});
+
+test('rotateInplace function exists', () => {
+  expect(rotateInplace).toBeDefined();
 });
 
 describe.each([
@@ -17,13 +23,35 @@ describe.each([
       [8, 5, 2],
       [9, 6, 3]
     ]
+  ],
+  [
+    [
+      [1, 2, 3, 4],
+      [5, 6, 7, 8],
+      [9, 10, 11, 12],
+      [13, 14, 15, 16]
+    ],
+    4,
+    [
+      [13, 9, 5, 1],
+      [14, 10, 6, 2],
+      [15, 11, 7, 3],
+      [16, 12, 8, 4]
+    ]
   ]
-])(`do stuff`, (a, n, expected) => {
-  test(`something`, ()=> {
-    console.log({
-      a:a,
-      n:n
-    });
+])(`Rotate, rotates an array 90 degrees`, (a, n, expected) => {
+  test(`rotate`, ()=> {
+    // console.log({
+    //   a:a,
+    //   n:n
+    // });
     expect(rotate(a, n)).toEqual(expected);
+  });
+  test.skip(`rotate`, ()=> {
+    // console.log({
+    //   a:a,
+    //   n:n
+    // });
+    expect(rotateInplace(a, n)).toEqual(expected);
   });
 });
