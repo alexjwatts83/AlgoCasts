aa7ed28f8e940165cc7a66175ae31261d786e2ec
alexwatts83 <alexjwatts83@gmail.com>
2020-04-10 14:47:45 +1000

    added some other single number solutions from leetcode

--- exercises/singleNumber/index.js
+++ exercises/singleNumber/index.js
@@ -1,15 +1,50 @@
+// var singleNumber = function(nums) {
+//   if (nums.length === 1) {
+//     return nums[nums.length -1];
+//   }
+//   let leCache= {};
+//   console.log({leCache:leCache});
+//   for(let i = nums.length -1; i >= 0; i--) {
+//       let num = nums[i];
+//       let key = `${num}`;
+//       leCache[key] = (leCache[key] === undefined ? 0 : leCache[key]) + 1;
+//       console.log({leCache:leCache, i: i, num: num, key: key});
+//   }
+//   console.log({leCache:leCache});
+//   for(var key in leCache) {
+//     console.log({key:key});
+//     if(leCache[key] === 1) {
+//       return parseInt(key);
+//     }
+//   }
+//   return null;
+// };
+
+// 36ms
 var singleNumber = function(nums) {
-  let leCache= {};
-  for(let i = nums.length; i >= 0; i--) {
-      let num = nums[i];
-      leCache[num] = (leCache[num] === undefined ? 0 : leCache[num]) + 1;
+  let r = 0;
+  for(let i = 0; i < nums.length; i += 1) {
+      r ^= nums[i];
   }
-  for(var key in leCache) {
-    if(leCache[key] === 1) {
-      return parseInt(key);
-    }
-  }
-  return null;
+  return r;
 };
+
+// 44ms
+// var singleNumber = function(nums) {
+//   let map = new Map()
+//   for(let i = 0 ; i < nums.length ; i++){
+//       if(map.has(nums[i])) map.delete(nums[i])
+//       else map.set(nums[i], nums[i])
+//   }
+//   return map.keys().next().value
+// }
+
+// 56ms
+// var singleNumber = function(nums) {
+//   for(var i = 1; i < nums.length;i++){
+//       nums[0] = nums[0] ^ nums[i];
+//   }
+//   return nums[0];
+// };
 
 module.exports  = singleNumber;
--- exercises/singleNumber/test.js
+++ exercises/singleNumber/test.js
@@ -17,6 +17,14 @@ describe.each([
     [17,12,5,-6,12,4,17,-5,2,-3,2,4,5,16,-3,-4,15,15,-4,-5,-6],
     16
   ],
+  [
+    [-1],
+    -1
+  ],
+  [
+    [-1,-1,-2],
+    -2
+  ],
 ])(`single number`, (arr, expected) => {
   test(`Expect ${expected} for ${arr}`, () => {
     expect(fn(arr)).toEqual(expected);
