aea0364009f067a15b3bb1cfe0c77ab2cf08af29
alexwatts83 <alexjwatts83@gmail.com>
2020-04-21 22:33:29 +1000

    added insertion sort

--- exercises/bubblesort/index.js
+++ exercises/bubblesort/index.js
@@ -19,7 +19,8 @@ var bubbleSortMe = function(arr) {
   return arr;
 }
 var selectionSort = function(arr) {
-  // the smallest number gets inserted
+  // reverse of bubble where the smallest value
+  // is added to start
   let n = arr.length;
   for(let i = 0; i < n; i++) {
     let minIndex = i;
@@ -40,7 +41,24 @@ var selectionSort = function(arr) {
   return arr
 }
 
+var insertionSort = function(arr) {
+  // insert value in the left section until its sorted
+  let n = arr.length;
+
+	let currentVal;
+  for(let i = 1; i < n; i++){
+    currentVal = arr[i];
+    let j;
+    for(j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j+1] = arr[j]
+    }
+    arr[j+1] = currentVal;
+  }
+  return arr;
+}
+
 module.exports = {
   bubbleSort: bubbleSortMe,
-  selectionSort: selectionSort
+  selectionSort: selectionSort,
+  insertionSort: insertionSort
 }
