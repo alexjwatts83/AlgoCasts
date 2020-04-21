0e1061d9cf1233cfdbac4884a4bd626889e71b41
alexwatts83 <alexjwatts83@gmail.com>
2020-04-21 21:18:19 +1000

    small refactoring

--- exercises/bubblesort/index.js
+++ exercises/bubblesort/index.js
@@ -2,17 +2,17 @@ var bubbleSortMe = function(arr) {
   // largest value bubbles up
   let n = arr.length - 2;
   for(let i = n; i >= 0; i--){
-    let wasNoSwapped = true;
+    let noSwapsies = true;
     for(let j = 0; j <= i; j++){
-      let current = arr[j];
-      let next = arr[j + 1];
-      if (current > next) {
-        arr[j] = next;
-        arr[j + 1] = current;
-        wasNoSwapped = false;
+      if (arr[j] > arr[j + 1]) {
+        let valueAtJ = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = valueAtJ;
+        noSwapsies = false;
       }
     }
-    if (wasNoSwapped) {
+
+    if (noSwapsies) {
       break;
     }
   }
@@ -22,25 +22,24 @@ var selectionSort = function(arr) {
   // the smallest number gets inserted
   let n = arr.length;
   for(let i = 0; i < n; i++) {
-    let min = arr[i];
     let minIndex = i;
     for(let j = i + 1; j < n; j++) {
-      let valueAtIndexJ = arr[j];
-      if (valueAtIndexJ < min) {
+      if (arr[j] < arr[minIndex]) {
         minIndex = j;
-        min = valueAtIndexJ;
       }
     }
+    
     if (i === minIndex) {
       continue;
     }
+
     let valueAtI = arr[i];
-    let valueAtMinIndex = arr[minIndex];
+    arr[i] = arr[minIndex];
     arr[minIndex] = valueAtI;
-    arr[i] = valueAtMinIndex;
   }
   return arr
 }
+
 module.exports = {
   bubbleSort: bubbleSortMe,
   selectionSort: selectionSort
